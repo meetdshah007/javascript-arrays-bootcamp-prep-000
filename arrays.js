d644382fd272f9e00b7cd8bcbbe368d33233ea74
Meet Shah <meetdshah007@gmail.com>
2019-02-19 14:26:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,8 @@ function addElementToBeginningOfArray(arr, elem) {
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, elem) {
-  return arr.splice(0, -1, elem);
+  arr.splice(0, -1, elem);
+  return arr;
 }
 
 function addElementToEndOfArray(arr, elem) {
