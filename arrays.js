dade5baef18266681b881205911c4a0b837900a8
Meet Shah <meetdshah007@gmail.com>
2019-02-19 14:19:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,23 +2,14 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, elem) {
-  arr.splice()
+  // arr.splice(0, -1, elem);
+  return [elem, ...arr]
 }
 
-  describe('addElementToBeginningOfArray(array, element)', () => {
-    it('adds an element to the beginning of an array', () => {
-      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
-    })
-
-    it('does not alter the original array', () => {
-      const array = [1]
-
-      addElementToBeginningOfArray(array, 'foo')
-
-      expect(array).to.eql([1])
-    })
-  })
-
+function destructivelyAddElementToBeginningOfArray(arr, elem){
+  
+}
+ 
   describe('destructivelyAddElementToBeginningOfArray(array, element)', () => {
     it('adds an element to the beginning of an array', () => {
       expect(destructivelyAddElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
