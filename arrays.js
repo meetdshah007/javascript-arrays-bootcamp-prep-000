0c3f7c5f6c99f25f1275fc8529ccf2997af56284
Meet Shah <meetdshah007@gmail.com>
2019-02-19 14:26:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,40 +30,10 @@ function removeElementFromBeginningOfArray(arr) {
   return arr.slice(0, 1);
 }
 
-  describe('removeElementFromBeginningOfArray(array)', () => {
-    it('removes the first element from the array', () => {
-      expect(removeElementFromBeginningOfArray([1, 2, 3])).to.eql([2, 3])
-    })
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  return arr.splice(arr.length - 1, 1);
+}
 
-    it('does not alter the original array', () => {
-      const array = [1, 2, 3];
-
-      removeElementFromBeginningOfArray(array);
-
-      expect(array).to.eql([1, 2, 3]);
-    })
-  })
-
-  describe('destructivelyRemoveElementFromEndOfArray(array)', () => {
-    it('returns the array with the last element removed', () => {
-      expect(destructivelyRemoveElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
-    })
-    
-    it('alters the original array', ()=>{
-      const array = [1, 2, 3];
-      destructivelyRemoveElementFromEndOfArray(array);
-      expect(array).to.eql([1, 2]);
-    })
-  })
-  
-  describe('removeElementFromEndOfArray(array)', () => {
-    it('removes the last element from the array', () => {
-      expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
-    })
-    
-    it('does not alter the original array', () => {
-      const array = [1, 2, 3];
-      removeElementFromEndOfArray(array);
-      expect(array).to.eql([1, 2, 3]);
-    })
-  })
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(arr.length - 1, arr.length);
+}
