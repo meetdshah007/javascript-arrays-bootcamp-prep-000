4501b486f7b1378a7488a2ce43228e0e1dcb29b6
Meet Shah <meetdshah007@gmail.com>
2019-02-19 14:18:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, elem) {
-  arr.slice(o, )
+  arr.splice()
 }
 
   describe('addElementToBeginningOfArray(array, element)', () => {
